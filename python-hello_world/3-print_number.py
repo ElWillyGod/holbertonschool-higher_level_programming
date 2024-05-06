24d63174e88d47d4929600a3eedd8e452848a1d1
root <manuelantognazza@gmail.com>
2024-05-06 19:48:13 -0300

    python styles

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 number = 98
 
+
 def print_int():
     print(f"{number} Battery street")
 
-print_int();
+
+print_int()
--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 number = 3.14159
 
+
 def print_float():
-    print(f"Float: {number:.2f}\n")
+    print(f"Float: {number:.2f}")
+
 
 print_float()
