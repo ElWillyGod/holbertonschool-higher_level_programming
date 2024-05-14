fd2c23ffdddd0a2141aa6592577b263a493d1cf5
root <manuelantognazza@gmail.com>
2024-05-14 17:01:25 -0300

    magic

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if (matrix in None) or (not matrix):
+    if (matrix == None) or (not matrix):
         return
 
     for i in matrix:
