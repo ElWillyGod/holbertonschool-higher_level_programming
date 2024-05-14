f586a9dec74c93022d81ec4529f3238cd1251023
root <manuelantognazza@gmail.com>
2024-05-14 17:12:50 -0300

    magic

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None or not matrix:
-        return
-
+    
     for i in matrix:
-        if not i:
-            continue
-
         for j in i:
-            print("{:d}".format(j), end="")
-            print(' ', end='') if j != i[-1] else print()
+            print("{:d}".format(j), end='')
+            if j != i[-1]:
+                print(" ", end='')
 
+        print("")
