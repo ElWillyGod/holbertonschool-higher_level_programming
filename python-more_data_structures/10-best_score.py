99a02e20d524d241f4e2606aff0fb8e6ccfccfd3
root <manuelantognazza@gmail.com>
2024-05-15 18:13:39 -0300

    check 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
-    maxInt = max(a_dictionary.values())
+    maxInt = max(a_dictionary.values(), default=None)
     for i, j in a_dictionary.items():
         if j == maxInt:
             return i
