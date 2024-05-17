936008bc492f6ce88dae5010aac43824b4d1794a
root <manuelantognazza@gmail.com>
2024-05-17 14:59:34 -0300

    multiple error

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    list_ret = []
+
+    for i in range(list_length):
+        try:
+            list_ret.append(my_list_1[i] / my_list_2[i])
+        except TypeError:
+            print("wrong type")
+            list_ret.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            list_ret.append(0)
+        except IndexError:
+            print("out of range")
+            list_ret.append(0)
+        finally:
+            pass
+
+    return list_ret
