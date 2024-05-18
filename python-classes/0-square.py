67ce8b867827c7371e387c73b198c2732e8dffdf
root <manuelantognazza@gmail.com>
2024-05-18 13:25:21 -0300

    styles de mierda, no los entiendooo

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
+'''Square:
+    ????
+'''
 
-"""
-Square:
-    no entiend los estilos
-"""
 
 class Square:
-
-    """
-    Example class with a docstring.
-
-    Attributes:
-        None.
-
-    Methods:
-        __init__(self): metodo?
-
-    """
-    def __init__(self):
-        pass
+    '''
+    no eniendo los los comentarios
+    '''
+    pass
