e3200364fb508e8c410d67f382edd663d19f8ab4
root <manuelantognazza@gmail.com>
2024-05-18 13:54:35 -0300

    style, no los entiendooo

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,14 +3,13 @@
     Square:
         ss
 """
-
 class Square:
 
     """__init__
 
     Attributes:
         __size (int): tamanio del cuadrado
-
     """
+
     def __init__(self, size):
         self.__size = size 
