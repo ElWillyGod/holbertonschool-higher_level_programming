15638748d9971aa7b9ade6cdffa788d5644cd264
root <manuelantognazza@gmail.com>
2024-05-18 14:47:19 -0300

    valide size square

---
+++ python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    Square:
+        ss
+"""
+
+
+class Square:
+    """__init__
+    metodo de inicio
+
+    Attributes:
+        __size (int): The size of square(private attribute).
+    """
+
+    def __init__(self, size):
+        """
+        Args:
+            size (int): tamanio del cuadrado
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
