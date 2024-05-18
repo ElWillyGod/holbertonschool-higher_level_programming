d9db593f01255fa8aff114ca27c15b27b07ad01e
root <manuelantognazza@gmail.com>
2024-05-18 13:42:11 -0300

    style, no los entiendooo

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+    Square:
+        ss
+"""
 
 class Square:
-
+    """
+    Args:
+        size: ni idea
+    """
     def __init__(self, size):
         self.__size = size 
