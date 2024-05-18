511f8195d5aa0a94ea020c0ed9b099a36809916f
root <manuelantognazza@gmail.com>
2024-05-18 13:09:52 -0300

    styles de mierda

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""
+'''
 Square:
     cuadrado
-"""
+'''
 
 class Square:
-    """
+    '''
     el ejemplo que pusieron de la documentacion es una cagada
-    """
+    '''
     def __init__(self):
         pass
