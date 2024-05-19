45d4c06850239bdcbd97bea1938c818612e8bc32
root <manuelantognazza@gmail.com>
2024-05-19 11:43:42 -0300

    print square

---
+++ python-classes/5-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+    Square:
+        ss
+"""
+
+
+class Square:
+    """__init__
+    metodo de inicio
+
+    Attributes:
+        __size (int): The size of square(private attribute).
+    """
+
+    def __init__(self, size=0):
+        """
+        Args:
+            size (int): tamanio del cuadrado.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Returns:
+            retun size Square
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        Args:
+            value (int): tamanio del cuadrado
+        """
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """
+        Returns:
+            return the area square
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the arguments"""
+        for i in range(self.__size):
+            [print("#", end='') for j in range(self.__size)]
+            print("")
+
+            if self.__size == 0:
+                print("")
