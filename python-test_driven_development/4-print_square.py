ddf9decea5318fbff2abfc4fa2a4cfbf19c2517d
root <manuelantognazza@gmail.com>
2024-05-21 19:36:08 -0300

    test 4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" print square #"""
+
+
+def print_square(size):
+    """square print #"""
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end='')
+        print()
