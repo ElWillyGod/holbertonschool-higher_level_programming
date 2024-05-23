ec716f2a532adfff1a997122ef9c9ec8c2811580
root <manuelantognazza@gmail.com>
2024-05-23 17:03:28 -0300

    2 is same class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""validation of type objet"""
+
+def is_same_class(obj, a_class):
+    """verifiqued in type"""
+
+    if isinstance(obj, a_class):
+        return True
+
+    return False
+
