da6790d73fa3a4c1158f8c4f4d6631c3da78fe63
root <manuelantognazza@gmail.com>
2024-05-23 20:43:32 -0300

    4 inherits

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@
 def inherits_from(obj, a_class):
     """medio mareado"""
 
-    return type(obj) is a_class and not isinstance(obj, a_class)
+    return type(obj) is not a_class and isinstance(obj, a_class)
