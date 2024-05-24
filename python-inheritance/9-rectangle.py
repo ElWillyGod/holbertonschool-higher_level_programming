1384d0d7e28b19e0de488b9600505ffbbbdae917
root <manuelantognazza@gmail.com>
2024-05-23 22:49:59 -0300

    rectangle str

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""empty class"""
+
+
+from _typeshed import Self
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__heigth = height
+
+    def area(self):
+        """area in Rectangle"""
+
+        return self.__width * self.__heigth
+
+    def __str__(self):
+        """print format [Rectangle] 3/5"""
+
+        return str(f"[Rectangle] {self.__width}/{self.__heigth}")
