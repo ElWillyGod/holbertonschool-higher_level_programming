b2615bd2159a428c0f981b1565bfcc02b4cf21e7
root <manuelantognazza@gmail.com>
2024-05-26 20:07:19 -0300

    iter

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -9,16 +9,15 @@ class CountedIterator:
         """iterar objeto y implementar contador"""
 
         self.itera = iter(objIter)
-        self.__counter = 0
+        self.counter = 0
 
-    @property
     def get_count(self):
         """geter in conut"""
-        return self.__counter
+        return self.counter
 
     def __next__(self):
         """return next item"""
-        self.__counter += 1
+        self.counter += 1
 
         try:
             return next(self.itera)
