a74067c93fd229fa79f9b3f4497ad74c1a1050ff
root <manuelantognazza@gmail.com>
2024-05-28 20:46:17 -0300

    dump

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,4 +6,4 @@ def write_file(filename="", text=""):
     """open file and retun the numbers of characters written"""
 
     with open(filename, encoding='UTF8') as file:
-        return len(file.readlines())
+        return file.write(text)
