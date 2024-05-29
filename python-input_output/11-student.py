2eafb933cd77c4826431fdd49db69b5568533ec4
root <manuelantognazza@gmail.com>
2024-05-28 22:20:02 -0300

    el 11 es mas facil que el 10

---
+++ python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""class Student"""
+
+
+class Student:
+    """__init__"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to to_json"""
+
+        if not isinstance(attrs, list):
+            return self.__dict__
+
+        return {attr: getattr(self, attr)
+                for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        """remplasta los directorios"""
+
+        for attr in json:
+            self.__dict__[attr] = json[attr]
