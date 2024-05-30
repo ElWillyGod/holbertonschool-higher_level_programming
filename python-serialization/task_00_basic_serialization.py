33f1ff3bf5c9d77f1c1db6839d7449126cbc873c
root <manuelantognazza@gmail.com>
2024-05-30 00:09:48 -0300

    Serialization

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""serialzation"""
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    # Your code here to serialize and save data to the specified file
+    with open(filename, "w") as file:
+        json.dump(data, file)
+
+
+def load_and_deserialize(filename):
+    # Your code here to load and deserialize data from the specified file
+    with open(filename, 'r') as file:
+        dataObjet = json.load(file)
+
+    return dataObjet
