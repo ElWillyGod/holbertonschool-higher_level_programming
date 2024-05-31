28e310c0e9f3381620dd36a61e72cd73d546f053
root <manuelantognazza@gmail.com>
2024-05-30 21:15:17 -0300

    Serialization

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""pickle"""
+import pickle
+
+
+class CustomObject:
+    """objet custom"""
+
+    def __init__(self, name, age, is_student):
+        self.__name__ = name
+        self.__age__ = age
+        self.__is_student__ = is_student
+
+    def display(self):
+        """print self"""
+
+        print(f"Name: {self.__name__}\n
+              Age: {self.__age__}\nIs Student: {self.__is_student__}")
+
+    def serialize(self, filename):
+        """serialize filename"""
+
+        with open(filename, 'wb') as fileText:
+            pickle.dump(self, fileText)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """deserialize filename"""
+
+        try:
+            with open(filename, 'rd') as fileText:
+                objet = pickle.load(fileText)
+        except ValueError:
+            return None
+
+        return objet
