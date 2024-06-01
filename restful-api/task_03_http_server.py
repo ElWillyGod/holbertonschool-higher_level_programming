549b62ca52c062959445e1cf1975d21809600642
root <manuelantognazza@gmail.com>
2024-06-01 00:43:19 -0300

    server HTTPs basic

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Basic HTTP Server"""
+import http.server
+import json
+
+
+class CustomRequestHandler(http.server.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
+
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(
+                {"name": "John", "age": 30, "city": "New York"}).encode())
+
+        elif self.path == '/info':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(
+                {"version": "1.0", "description":
+                 "A simple API built with http.server"}).encode())
+
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(
+                {"error": "Endpoint not found"}).encode())
+
+
+def run(server_class=http.server.HTTPServer,
+        handler_class=CustomRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+
+run()
