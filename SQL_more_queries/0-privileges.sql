503904ee58c32bb645ba8574f9604fa03ef6a941
root <manuelantognazza@gmail.com>
2024-06-21 19:36:18 -0300

    SHOW GRANTS

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- previleges of the MySQL users
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost' AND 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost' AND FOR 'user_0d_2'@'localhost';
 -- SHOW GRANTS FOR 'user_0d_2'@'localhost';
