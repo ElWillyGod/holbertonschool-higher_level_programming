d830013962562a75e4f7b53d2a2859a0077d194a
root <manuelantognazza@gmail.com>
2024-06-22 12:37:43 -0300

    FOREIGN KEY

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 CREATE TABLE hbtn_0d_usa.cities (id INT UNIQUE NOT NULL AUTO_INCREMENT,
-				state_id NOT NULL,
+				state_id INT NOT NULL,
 				name VARCHAR(256) NOT NULL,
 				PRIMARY KEY(id),
 				FOREIGN KEY(state_id) REFERENCES states(id)
