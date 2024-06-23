d7f2118db1f1ed8e4dbb8c794a1e839eb5793d8a
root <manuelantognazza@gmail.com>
2024-06-23 18:02:45 -0300

    lo mismo que el 1 pero con un WHERE

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -26,7 +26,7 @@ def states(username, userPassword, database_name):
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id")
 
     rows = cur.fetchall()
 
