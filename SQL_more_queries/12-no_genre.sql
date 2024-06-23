a452fb3e1d3f60034805d93305f2fa69e3a490de
root <manuelantognazza@gmail.com>
2024-06-22 23:54:35 -0300

    LEFT JOIN and WHERE

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- lo mismo que el 11 pero con un where
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
