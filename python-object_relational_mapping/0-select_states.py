4eeccbbbc8523875cc3fec0779d40110b3778423
root <manuelantognazza@gmail.com>
2024-06-23 16:53:22 -0300

    connect python a SQL

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Write a script that lists all states from the database hbtn_0e_0_usa"""
+import sys
+import MySQLdb
+
+
+def states(username, userPassword, database_name):
+    '''ejecutar la consulta'''
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=userPassword,
+                         database=database_name)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+
+    db.close()
+
+
+if __name__ == '__main__':
+    '''execute query'''
+
+    user = sys.argv[1]
+    passw = sys.argv[2]
+    bd = sys.argv[3]
+
+    states(user, passw, bd)
