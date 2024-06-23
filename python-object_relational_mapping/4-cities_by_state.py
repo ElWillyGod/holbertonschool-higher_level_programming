8a9328ef50fca93fce7a1b78782afb95f5f0c3e7
root <manuelantognazza@gmail.com>
2024-06-23 19:08:21 -0300

    consulta larga

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Write a script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+def states(username, userPassword, database_name):
+    """
+    Connects to a MySQL database and executes a query to retrieve all states.
+
+    Args:
+        username (str): The username to use for the database connection.
+        userPassword (str): The password to use for the database connection.
+        database_name (str): The name of the database to connect to.
+
+    Returns:
+        None
+
+    Raises:
+        MySQLdb._exceptions.OperationalError: If the database connection fails.
+    """
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=userPassword,
+                         db=database_name)
+
+    cur = db.cursor()
+
+    query = ("SELECT cities.id, cities.name, states.name " +
+             "FROM cities INNER JOIN states " +
+             "ON cities.state_id = states.id " +
+             "ORDER BY cities.id")
+
+    cur.execute(query)
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+
+    db.close()
+
+
+if __name__ == "__main__":
+    """mas cosas"""
+
+    user = sys.argv[1]
+    passw = sys.argv[2]
+    bd = sys.argv[3]
+
+    states(user, passw, bd)
