43126af6965b91075ba43b78eaf9fc5a083d9426
root <manuelantognazza@gmail.com>
2024-06-23 18:54:39 -0300

    paramentros en la query

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Write a script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+def states(username, userPassword, database_name, state_name):
+    """
+    Connects to a MySQL database and executes a query to retrieve all states.
+
+    Args:
+        username (str): The username to use for the database connection.
+        userPassword (str): The password to use for the database connection.
+        database_name (str): The name of the database to connect to.
+
+    Returns:
+        None
+
+    Raises:
+        MySQLdb._exceptions.OperationalError: If the database connection fails.
+    """
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=userPassword,
+                         db=database_name)
+
+    cur = db.cursor()
+
+    query = ("SELECT * FROM states " +
+             "WHERE name LIKE BINARY %s ORDER BY states.id")
+
+    cur.execute(query, (state_name,))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+
+    db.close()
+
+
+if __name__ == "__main__":
+    """mas cosas"""
+
+    user = sys.argv[1]
+    passw = sys.argv[2]
+    bd = sys.argv[3]
+    name = sys.argv[4]
+
+    states(user, passw, bd, name)
