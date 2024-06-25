516b583c2e44a660ef49b4ba0b61dc5a88daf1a5
root <manuelantognazza@gmail.com>
2024-06-24 21:34:35 -0300

    ORM

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,9 +7,11 @@ import sqlalchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
+
 
 class State(Base):
     __tablename__ = 'state'
     id = Column(Integer, primary_key=True)
-    name =  Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
