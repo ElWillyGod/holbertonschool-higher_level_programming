cd5c8e79410974b7fc8dbfef02e70c02507e57eb
root <manuelantognazza@gmail.com>
2024-06-24 23:32:35 -0300

    ORM 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""ORM pa buena"""
+
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+from model_state import Base, State
+
+
+def listState(user, passw, dbName, name):
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, passw, dbName), pool_pre_ping=True)
+
+    session = Session(engine)
+
+    state = session.query(State).filter(State.name == name).first()
+
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print('Not found')
+
+    session.close()
+
+
+if __name__ == '__main__':
+    us = argv[1]
+    pas = argv[2]
+    db = argv[3]
+    name = argv[4]
+
+    listState(us, pas, db, name)
