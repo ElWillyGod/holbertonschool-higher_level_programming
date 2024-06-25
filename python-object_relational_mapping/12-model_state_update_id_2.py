5b05b534c8d704457963ff1bdbe9a59b2054cfd8
root <manuelantognazza@gmail.com>
2024-06-24 23:48:56 -0300

    ORM 12

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""ORM pa buena"""
+
+
+from os import name
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+from model_state import Base, State
+
+
+def listState(user, passw, dbName):
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, passw, dbName), pool_pre_ping=True)
+
+    session = Session(engine)
+
+    state = session.query(State).filter(State.id == '2').first()
+
+    if state:
+        state.name = 'New Mexico'
+        session.commit()
+
+    session.close()
+
+
+if __name__ == '__main__':
+    us = argv[1]
+    pas = argv[2]
+    db = argv[3]
+
+    listState(us, pas, db)
