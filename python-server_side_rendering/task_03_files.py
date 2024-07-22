4d7e1ac665d77ff3aca803e7b7e8d519af512d31
root <manuelantognazza@gmail.com>
2024-07-21 23:25:03 -0300

    03 v1

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
+import csv
 
 
 app = Flask(__name__)
@@ -24,5 +25,31 @@ def items():
 
     return render_template('items.html', items=items.get('items', []))
 
+@app.route('/products')
+def products():
+    source = request.args.get('source')
+    id = request.args.get('id')
+    products = []
+    
+    if source == 'json':
+        with open('products.json', 'r') as f:
+            products = json.load(f)
+            
+    elif source == 'csv':
+        with open('products.csv', 'r') as f:
+            data = csv.DictReader(f)
+            for row in data:
+                products.append(row)
+    else:
+        return render_template('product_display.html', errorMessage="Wrong source")
+    
+    if id:
+        products = [product for product in products if product['id'] == id]
+        
+        if products is None:
+                return render_template('product_display.html', errorMessage="Product not found")
+    
+    return render_template('product_display.html', products=products)
+    
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
