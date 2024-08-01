fea001587c2a410468300f54828fa066eaefdef0
root <manuelantognazza@gmail.com>
2024-07-31 22:50:16 -0300

    como tan muchachos

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,1 +1,6 @@
-/*  ya veremos */
+fetch("https://swapi-api.hbtn.io/api/people/5/?format=json").then((res) => {
+  return res.json();
+})
+.then((data) => {
+    document.querySelector('#character').textContent = data.name;
+});
